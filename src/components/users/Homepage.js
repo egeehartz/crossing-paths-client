871c818d54d99bc90bc4bdecde59f32cc452a697
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-10 10:15:22 -0600

    added reactstrap to add a modal

--- src/components/users/Homepage.js
+++ src/components/users/Homepage.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
+import {Modal, ModalBody, ModalHeader, Button} from "reactstrap"
 import { CategoryContext } from "../designs/CategoryProvider"
 import { DesignContext } from "../designs/DesignProvider"
 import { UserContext } from "./UserProvider"
@@ -8,7 +9,7 @@ import { UserContext } from "./UserProvider"
 export const Homepage = () => {
     const { categories, getCategories } = useContext(CategoryContext)
     const { getDesignByUser } = useContext(DesignContext)
-    const {getCurrentUser} = useContext(UserContext)
+    const { getCurrentUser } = useContext(UserContext)
 
     const [userDesigns, setUserDesigns] = useState([])
 
@@ -16,11 +17,12 @@ export const Homepage = () => {
     useEffect(() => {
         getCategories()
         getCurrentUser()
-        .then((user) => getDesignByUser(user.id))
-        .then(setUserDesigns)
+            .then((user) => getDesignByUser(user.id))
+            .then(setUserDesigns)
     }, [])
 
- console.log(userDesigns)
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
 
 
     return (
@@ -28,8 +30,8 @@ export const Homepage = () => {
             <h1>Homepage</h1>
             <div>
                 {
-                categories.map(c => <div>{c.label}</div>)
-            }
+                    categories.map(c => <div>{c.label}</div>)
+                }
             </div>
             <br />
             <div>
@@ -38,15 +40,19 @@ export const Homepage = () => {
                         return <div>
                             <h4>{d.title}</h4>
                             {d.design_img === null ?
-                              "no image": <p>{d.design_image}</p>}
-                            </div>
+                                "no image" : <p>{d.design_image}</p>}
+                        </div>
                     })
                 }
             </div>
-            <button>+</button>
-            <dialog>
-                
-            </dialog>
+            <button onClick={toggle}>+</button>
+            <Modal isOpen={modal} toggle={toggle} >
+                <ModalHeader toggle={toggle}>Add a Design!</ModalHeader>
+                <ModalBody>
+                    <Button color="primary" onClick={toggle}>Create a New One</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Upload an Existing design</Button>
+                </ModalBody>
+            </Modal>
         </>
     )
 }
