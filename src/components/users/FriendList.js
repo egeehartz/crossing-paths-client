971d5cc14b6c3b3275b7e3ad7b2d84025c68b4a7
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-11 09:58:17 -0600

    clicking on friend name goes to their profile

--- src/components/users/FriendList.js
+++ src/components/users/FriendList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import { FollowingsContext } from "./FriendProvider"
 import { UserContext } from "./UserProvider"
 import defaultImg from "./images/default.png"
@@ -28,11 +29,13 @@ export const FriendList = () => {
             {
                 friends.map(f => {
                     return <div>
-                        {f.friend.profile_img === "" || f.friend.profile_img === null
+                        {f.friend.profile_img === null || f.friend.profile_img === undefined
                     ? <img src={defaultImg} alt="default avatar smiley" width='50px' />
                     : <img src={f.friend.profile_img} width='50px' />
                 }
+                        <Link to={{ pathname: `/profile/${f.friend.id}` }}>
                         {f.friend.full_name}
+                        </Link>
                         </div>
                 })
             }
