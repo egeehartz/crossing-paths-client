bfad482fca107508bf54b610cfb05cc79171aad2
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-10 22:02:58 -0600

    tried to add a tooltip, changed my mind

--- src/components/designs/HomeDesigns.js
+++ src/components/designs/HomeDesigns.js
@@ -1,6 +1,6 @@
-import React from "react"
-import {useLocation} from "react-router-dom"
-import {Button} from "reactstrap"
+import React, {useState} from "react"
+import { useLocation } from "react-router-dom"
+import { Button, Tooltip } from "reactstrap"
 import "./HomeDesigns.css"
 
 
@@ -8,6 +8,12 @@ import "./HomeDesigns.css"
 
 export const HomeDesigns = ({ design }) => {
     const location = useLocation()
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+
+    const clickMe = () => {
+        window.alert('you can click me!')
+    }
 
     return (
         <>
@@ -15,29 +21,33 @@ export const HomeDesigns = ({ design }) => {
                 <div className="flip-card-inner">
                     <div className="flip-card-front">
                         <h4>{design.title}</h4>
-                        <img className="image" src={design.design_img}  />
+                        <img className="image" src={design.design_img} />
                     </div>
+
+
                     <div className="flip-card-back">
                         <h4>{design.title}</h4>
                         {/* posted by logic */}
                         {location.pathname === "/explore" ?
-                            <p>posted by {design.user.full_name}</p> 
+                            <p>posted by {design.user.full_name}</p>
                             : ""}
-                        <img className="image" src={design.design_img}  />
+                        <img className="image" src={design.design_img} />
                         <p>{design.category.label}</p>
 
-                        {design.link !== "" ? 
-                           <a className="design_link"
-                           href={design.link} target="_blank">source</a> :
-                           location.pathname === "/explore" & design.link === "" ? 
-                            "" :
-                            location.pathname !== "/explore" & design.link === "" ? 
-                            <p>add link</p> : ""}
+                        {design.link !== "" ?
+                            <a className="design_link"
+                                href={design.link} target="_blank">source</a> :
+                            location.pathname === "/explore" & design.link === "" ?
+                                "" :
+                                location.pathname !== "/explore" & design.link === "" ?
+                                    <p>add link</p> : ""}
 
                         {/* add to board logic */}
                         {location.pathname === "/explore" ?
-                            <Button>+</Button> 
+                            <Button>+</Button>
                             : ""}
+
+                       
                     </div>
                 </div>
             </div>
