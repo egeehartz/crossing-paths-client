4130d16f41e4964a66fc783bd83b5756a7bf6940
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-14 13:53:57 -0600

    friends search works beautifully

--- src/components/users/FriendList.js
+++ src/components/users/FriendList.js
@@ -8,8 +8,8 @@ import defaultImg from "./images/default.png"
 
 
 export const FriendList = () => {
-    const {getCurrentUser, getUsers, users, getUsersToFollow} = useContext(UserContext)
-    const {getFriendsByFollower} = useContext(FollowingsContext)
+    const { getCurrentUser, getUsers, users, getUsersToFollow } = useContext(UserContext)
+    const { getFriendsByFollower, createFollowing } = useContext(FollowingsContext)
 
     const [friends, setFriends] = useState([])
     const [potentialFriends, setPotentialFriends] = useState([])
@@ -19,57 +19,69 @@ export const FriendList = () => {
     const handleChange = event => {
         const userInput = event.target.value.toLowerCase()
         setSearchTerm(userInput);
-      };
+    };
 
     useEffect(() => {
         if (searchTerm !== "") {
             const results = potentialFriends.filter(u =>
                 u.user.username.toLowerCase().includes(searchTerm)
             )
-        
+
             setSearchResults(results);
         }
     }, [searchTerm])
-    
-    
+
+
     useEffect(() => {
         getUsers()
         getCurrentUser()
-        .then(user => {
-            getFriendsByFollower(user.id)
-            .then(setFriends)
-        })
-        .then(() => {
-            getUsersToFollow().then(setPotentialFriends)
-        })
-    },[])
+            .then(user => {
+                getFriendsByFollower(user.id)
+                    .then(setFriends)
+            })
+            .then(() => {
+                getUsersToFollow().then(setPotentialFriends)
+            })
+    }, [])
 
 
     return (
         <>
             <h1>Friends</h1>
             <div>
-            {
-                friends.map(f => {
-                    return <div>
-                        {f.friend.profile_img === null || f.friend.profile_img === undefined
-                    ? <img src={defaultImg} width='50px' alt="profile" />
-                    : <img src={f.friend.profile_img} width='50px' alt="profile" />
-                }
-                        <Link to={{ pathname: `/profile/${f.friend.id}`, state:{friendObj: f} }}>
-                        {f.friend.full_name}
-                        </Link>
+                {
+                    friends.map(f => {
+                        return <div>
+                            {f.friend.profile_img === null || f.friend.profile_img === undefined
+                                ? <img src={defaultImg} width='50px' alt="profile" />
+                                : <img src={f.friend.profile_img} width='50px' alt="profile" />
+                            }
+                            <Link to={{ pathname: `/profile/${f.friend.id}`, state: { friendObj: f } }}>
+                                {f.friend.full_name}
+                            </Link>
                         </div>
-                })
-            }
+                    })
+                }
             </div>
             <div>
                 <h2>Find Friends!</h2>
                 <input type="text" value={searchTerm} onChange={handleChange} placeholder="search by username" />
                 <ul>
-                    { searchTerm !== "" ?
+                    {searchTerm !== "" ?
                         searchResults.map(result => {
-                            return <> <li>{result.user.username}</li> <button>follow</button> </>
+                            return <> <li>{result.user.username}</li>
+                                <button onClick={() => {
+                                    createFollowing({
+                                        friendId: result.id
+                                    })
+                                        .then(() =>
+                                            getCurrentUser()
+                                            .then((user) => {
+                                                getFriendsByFollower(user.id)
+                                                    .then(setFriends)
+                                                    .then(setSearchTerm(""))
+                                            }))
+                                }}>follow</button> </>
                         }) : ""
                     }
                 </ul>
