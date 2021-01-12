d2be9c63252da655ae987da9db147ee011a24d71
Emily Hartzell <emily.g.hartzell@gmail.com>
2021-01-11 20:48:33 -0600

    commented code, user can't follow someone twice from /explore, follow modal say's user's name

--- src/components/designs/ExploreList.js
+++ src/components/designs/ExploreList.js
@@ -10,12 +10,14 @@ export const ExploreList = () => {
     const {getExploreDesigns} = useContext(DesignContext)
     const {categories, getCategories} = useContext(CategoryContext)
     const [eDesigns, setExploreDesigns] = useState([])
+    const [changeHeard, setChange] = useState(true)
+    const toggleChange = () => {changeHeard ? setChange(false) : setChange(true)}
 
     useEffect(() => {
         getCategories()
         getExploreDesigns()
             .then(res => setExploreDesigns(res))
-    },[])
+    },[changeHeard])
 
 
     return(
@@ -23,7 +25,7 @@ export const ExploreList = () => {
         <div className="exploreDiv">
             {
                 eDesigns.map(d => {
-                    return <DesignList key={d.id} design={d} categories={categories} />
+                    return <DesignList key={d.id} design={d} categories={categories} func={toggleChange} />
                 })
             }
         </div>
